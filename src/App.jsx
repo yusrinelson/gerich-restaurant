e53956e23acd7c6238280f2c38dea8c2f295387f
Yusri Nelson <yusrinelson17@gmail.com>
2024-05-15 18:31:33 +0200

    created folder structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css'
+
+export default function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  )
+}
