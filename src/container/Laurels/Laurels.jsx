af3bbf3c612b147bf46598e079de52616fdfbf63
Yusri Nelson <yusrinelson17@gmail.com>
2024-05-18 17:27:21 +0200

    created the gallery section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
-
+import { images, data } from '../../constants';
+import { SubHeading } from '../../components';
+import PropTypes from "prop-types"
 import './Laurels.css';
 
+const AwardCard = ({award: {imgUrl, title, subtitle}}) =>{
+  return(
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award" />
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{color: "#DCCA67"}} title={title}></p>
+      <p className='p__cormorant'>{subtitle}</p>
+    </div>
+  </div>
+  )
+}
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Awards & recognition'/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+
+      <div className='app__laurels_awards'>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title}/>)}
+      </div>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
 export default Laurels;
+
+AwardCard.propTypes = {
+  award: PropTypes.shape({
+    imgUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }).isRequired,
+};
